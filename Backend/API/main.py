510534a78ea54f4dd4734bed7c3e73cbd98e2ea8
Lucia <Luciaabouhamad@gmail.com>
2024-03-27 20:11:04 +0100

    put item

--- Backend/API/main.py
+++ Backend/API/main.py
@@ -52,6 +52,27 @@ def add_item(item: Item) -> dict[str, Item]:
     return {"added": item}
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.put("/update/{item_id}")
+def update(
+    item_id: int,
+    name: str | None = None,
+    price: float | None = None,
+    count: int | None = None,
+) -> dict[str, Item]:
+
+    if item_id not in items:
+        HTTPException(status_code=404, detail=f"Item with {item_id=} does not exist.")
+    if all(info is None for info in (name, price, count)):
+        raise HTTPException(
+            status_code=400, detail="No parameters provided for update."
+        )
+
+    item = items[item_id]
+    if name is not None:
+        item.name = name
+    if price is not None:
+        item.price = price
+    if count is not None:
+        item.count = count
+
+    return {"updated": item}
