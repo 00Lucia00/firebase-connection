c6d7ff53c1efb099f9a23cb93fd4c935f188d5ce
Lucia <Luciaabouhamad@gmail.com>
2024-03-27 20:04:07 +0100

    get all and by id

--- Backend/API/main.py
+++ Backend/API/main.py
@@ -19,20 +19,27 @@ class Item(BaseModel):
     category: Category
 
 #List
-    items = {
+items = {
     0: Item(name="Hammer", price=9.99, count=20, id=0, category=Category.TOOLS),
     1: Item(name="Pliers", price=5.99, count=20, id=1, category=Category.TOOLS),
     2: Item(name="Nails", price=1.99, count=100, id=2, category=Category.CONSUMABLES),
 }
-    
+
+# FastAPI handles JSON serialization and deserialization for us.
+# We can simply use built-in python and Pydantic types, in this case dict[int, Item].   
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def index() -> dict[str, dict[int, Item]]:
+
+    return {"items": items}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def query_item_by_id(item_id: int) -> Item:
+
+    if item_id not in items:
+        HTTPException(status_code=404, detail=f"Item with {item_id=} does not exist.")
+
+    return items[item_id]
 
 
 @app.put("/items/{item_id}")
